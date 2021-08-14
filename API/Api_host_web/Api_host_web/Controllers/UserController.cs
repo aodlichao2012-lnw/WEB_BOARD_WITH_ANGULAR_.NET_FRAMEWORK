6302fd2fcb52f4cea95242d4eb677ead4fb2e58c
aodlichao2012-lnw <52318749+aodlichao2012-lnw@users.noreply.github.com>
2021-08-14 19:52:14 +0700

    aaaa

--- API/Api_host_web/Api_host_web/Controllers/UserController.cs
+++ API/Api_host_web/Api_host_web/Controllers/UserController.cs
@@ -19,7 +19,7 @@ namespace Api_host_web.Controllers
         [HttpGet]
         public IHttpActionResult GetLogin(UserAcc user)
         {
-           
+            res = new DataTable();
             res = data.Get_form_sql("SELECT id , [username],[password] FROM [dbo].[User]");
 
             for (int i = 0; i < res.Rows.Count; i++)
@@ -27,6 +27,7 @@ namespace Api_host_web.Controllers
                 if (user.username == res.Rows[i]["username"].ToString() && user.password == res.Rows[i]["password"].ToString())
                 {
                     res = data.Get_form_sql("INSERT INTO [dbo].[SessionLog] ([session_name],[date_time_singin],[id_user]) VALUES ('" + res.Rows[i]["username"].ToString() + "' , " + DateTime.Now.ToString("yyyy-MM-dd") + "," + res.Rows[i]["id"].ToString() + ")");
+                
                     object ressponse = JsonConvert.SerializeObject(res);
                     return Ok(ressponse);
                 }
@@ -38,8 +39,7 @@ namespace Api_host_web.Controllers
         [HttpPost]
         public IHttpActionResult PostCreate(UserAcc user)
         {
-
-            DataTable res = new DataTable();
+            res = new DataTable();
             res = data.Get_form_sql("INSERT INTO [dbo].[User] ( [username],[password],[Email]) VALUES (  '" + user.username + "','" + user.password + "','" + user.Email + "')");
             object response = JsonConvert.SerializeObject(res);
             return Ok(response);
@@ -50,10 +50,55 @@ namespace Api_host_web.Controllers
         [HttpPost]
         public IHttpActionResult DeleteLogout(UserAcc user)
         {
-            DataTable res = new DataTable();
-            res = data.Get_form_sql("DELETE [dbo].[SessionLog] where  id_user = "+user.id+"");
+            res = new DataTable();
+            res = data.Get_form_sql("DELETE [dbo].[SessionLog] where  id_user = " + user.id + "");
             object response = JsonConvert.SerializeObject(res);
             return Ok(response);
+        }
+
+        [Route("Profile")]
+        [HttpGet]
+        public IHttpActionResult PostProfile([FromUri]string id)
+        {
+            res = new DataTable();
+            res = data.Get_form_sql(@"
+                                SELECT[id]
+                                        ,[post]
+                                        ,[datetimepost]
+                                        ,[like_post]
+                                        ,[like_post_count]
+                                        ,[edit_post]
+                                        ,[edit_post_count]
+                                        ,[User_id]
+                                    FROM[dbo].[Post_] where User_id = " + id + "");
+            object response = JsonConvert.SerializeObject(res);
+            return Ok(res);
+        }
+
+        [Route("Write_Profile")]
+        [HttpPost]
+        public IHttpActionResult PostWriteProfile(Profile pro)
+        {
+            res = new DataTable();
+
+            res = data.Get_form_sql(@"INSERT INTO [dbo].[Post_]
+                                    ([post]
+                                   ,[datetimepost]
+                                   ,[like_post]
+                                   ,[like_post_count]
+                                   ,[edit_post]
+                                   ,[edit_post_count]
+                                   ,[User_id])
+                             VALUES
+                                   ('"+pro.post+"',"+
+                                    ""+DateTime.Now.ToString("yyyy-MM-dd")+" "+
+                                   ",'"+pro.like_post+"'"+
+                                   ", "+pro.like_post_count+""+
+                                   ",'"+pro.edit_post+"'"+
+                                   ","+pro.edit_post_count+""+
+                                   ","+pro.User_id+") ");
+            object response = JsonConvert.SerializeObject(res);
+            return Ok(res);
         }
 
 
---
+++ API/Api_host_web/Api_host_web/Models/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api_host_web.Models
+{
+    public class Profile
+    {
+        public int id { get; set; }
+        public string post { get; set; }
+        public DateTime datetimepost { get; set; }
+        public bool like_post { get; set; }
+        public int like_post_count { get; set; }
+        public string edit_post { get; set; }
+        public int edit_post_count { get; set; }
+        public int User_id { get; set; }
+    }
+}

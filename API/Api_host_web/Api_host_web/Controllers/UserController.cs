40b7ee684fc11c5e2721a782fb2b0f280e7737f3
aodlichao2012-lnw <52318749+aodlichao2012-lnw@users.noreply.github.com>
2021-08-15 18:36:20 +0700

    20210815

--- API/Api_host_web/Api_host_web/Controllers/UserController.cs
+++ API/Api_host_web/Api_host_web/Controllers/UserController.cs
@@ -14,25 +14,48 @@ namespace Api_host_web.Controllers
     public class UserController : ApiController
     {
         database_helper data = new database_helper();
-        DataTable res = new DataTable();
+        DataTable res;
+        DataTable res2;
         [Route("User")]
         [HttpGet]
         public IHttpActionResult GetLogin(UserAcc user)
         {
             res = new DataTable();
             res = data.Get_form_sql("SELECT id , [username],[password] FROM [dbo].[User]");
-
             for (int i = 0; i < res.Rows.Count; i++)
             {
                 if (user.username == res.Rows[i]["username"].ToString() && user.password == res.Rows[i]["password"].ToString())
                 {
-                    res = data.Get_form_sql("INSERT INTO [dbo].[SessionLog] ([session_name],[date_time_singin],[id_user]) VALUES ('" + res.Rows[i]["username"].ToString() + "' , " + DateTime.Now.ToString("yyyy-MM-dd") + "," + res.Rows[i]["id"].ToString() + ")");
-                
-                    object ressponse = JsonConvert.SerializeObject(res);
-                    return Ok(ressponse);
+                    res2 = data.Get_form_sql("SELECT [session_name] FROM [dbo].[SessionLog] where session_name = '" + user.username + "'");
+                    if (res2.Rows.Count > 0 )
+                    {
+                        return BadRequest("Session has already");
+                    }
+                    else
+                    {
+                        GetAuthen(Convert.ToInt32(res.Rows[i]["id"]), user.username);
+                        object ressponse2 = JsonConvert.SerializeObject(res);
+                        return Ok(ressponse2);
+                    }
                 }
             }
-            return NotFound();
+
+                return BadRequest("User name or password Not match !!");
+        }
+
+        public void GetAuthen(int id, string username)
+        {
+            res = new DataTable();
+            try
+            {
+                res = data.Get_form_sql(@"INSERT INTO [dbo].[SessionLog] 
+                                        ([session_name],[date_time_singin],[id_user])
+                                         VALUES ('" + username + "' , " + DateTime.Now.ToString("yyyy-MM-dd") + "," + id + ")");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         [Route("Create")]
@@ -40,7 +63,9 @@ namespace Api_host_web.Controllers
         public IHttpActionResult PostCreate(UserAcc user)
         {
             res = new DataTable();
-            res = data.Get_form_sql("INSERT INTO [dbo].[User] ( [username],[password],[Email]) VALUES (  '" + user.username + "','" + user.password + "','" + user.Email + "')");
+            res = data.Get_form_sql(@"INSERT INTO [dbo].[User]
+                                    ( [username],[password],[Email])
+                                    VALUES (  '" + user.username + "','" + user.password + "','" + user.Email + "')");
             object response = JsonConvert.SerializeObject(res);
             return Ok(response);
         }
@@ -90,17 +115,37 @@ namespace Api_host_web.Controllers
                                    ,[edit_post_count]
                                    ,[User_id])
                              VALUES
-                                   ('"+pro.post+"',"+
-                                    ""+DateTime.Now.ToString("yyyy-MM-dd")+" "+
-                                   ",'"+pro.like_post+"'"+
-                                   ", "+pro.like_post_count+""+
-                                   ",'"+pro.edit_post+"'"+
-                                   ","+pro.edit_post_count+""+
-                                   ","+pro.User_id+") ");
+                                   ('" + pro.post + "'," +
+                                    "" + DateTime.Now.ToString("yyyy-MM-dd") + " " +
+                                   ",'" + pro.like_post + "'" +
+                                   ", " + pro.like_post_count + "" +
+                                   ",'" + pro.edit_post + "'" +
+                                   "," + pro.edit_post_count + "" +
+                                   "," + pro.User_id + ") ");
             object response = JsonConvert.SerializeObject(res);
             return Ok(res);
         }
 
+        [Route("AllUser")]
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            res = new DataTable();
+            res = data.Get_form_sql("SELECT id , [username],[password] FROM [dbo].[User]");
+            object respone = JsonConvert.SerializeObject(res);
+            return Ok(respone);
+        }
+
+
+        [Route("AllSession")]
+        [HttpGet]
+        public IHttpActionResult Getsession()
+        {
+            res = new DataTable();
+            res = data.Get_form_sql("SELECT *  FROM [dbo].[SessionLog]");
+            object respone = JsonConvert.SerializeObject(res);
+            return Ok(respone);
+        }
 
     }
 }
